c5c319c2c55e3bf36831b3df2ce126316656aa1c
Smit Patel <smitp802@gmail.com>
2021-02-02 19:02:41 -0500

    02 feb

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Form from './components/form.js'
+import TodoList from './components/TodoList.js'
 
 function App() {
+
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <header>
+          <h1>Smit's To-do List</h1>
+       </header>
+       <Form
+          inputText={inputText}
+          setInputText={setInputText}
+          todos={todos}
+          setTodos={setTodos} 
+       />
+       <TodoList setTodos={setTodos} todos={todos} />
     </div>
   );
 }
---
+++ src/components/Todo.js
@@ -0,0 +1,36 @@
+import { React } from "react";
+
+const Todo = ({ text, setTodos, todos, todo }) => {
+
+    const deleteHandler = () => {
+        //console.log(todo);
+        //filtering the todos-array and re-setting the todos-array with setTodos 
+        setTodos(todos.filter((el) => el.id !== todo.id));
+    };
+
+    const completeHandler = () => {
+        setTodos(
+            todos.map((item)=> {
+                if(item.id == todo.id){
+                    return {
+                        ...item, completed: !item.completed,
+                    };
+                }
+                return item;
+            })
+        )
+    };
+
+    return(
+        <div className="todo">
+            <li className="todo-item"> {text} </li>
+            <button onClick={completeHandler} className="complete-btn"> 
+                <i className="fas fa-check"></i>
+            </button>
+            <button onClick={deleteHandler} className="trash-btn">
+                <i className="fas fa-trash"></i>
+            </button>
+        </div>
+    );
+}
+export default Todo;
